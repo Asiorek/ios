b89d0de8b02b8d5817fda4df0f0722193a2d6e39
UŇľytkownik GoŇõńá <Guest@imac-07.local>
2016-10-20 10:57:22 +0200

    login connecting to ui

--- joanna/ViewController.swift
+++ joanna/ViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+// MARK: Properties
+    @IBOutlet weak var artistTextField: NSLayoutConstraint!
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var genreTextField: UITextField!
+    @IBOutlet weak var yearTextView: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+// MARK: Actions
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
