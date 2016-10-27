54e1b2432739a291473e7a07bbf5aa3c9e48f196
UŇľytkownik GoŇõńá <Guest@imac-14.local>
2016-10-27 11:00:25 +0200

    great tutor

--- joanna/ViewController.swift
+++ joanna/ViewController.swift
@@ -15,10 +15,27 @@ class ViewController: UIViewController {
     @IBOutlet weak var genreTextField: UITextField!
     @IBOutlet weak var yearTextView: UITextField!
     
+    @IBOutlet weak var numberRate: UILabel!
+
+    @IBOutlet weak var buttonRates: UIStepper!
+    @IBOutlet weak var buttonPrev: UIButton!
+    @IBOutlet weak var buttonNext: UIButton!
+    @IBOutlet weak var buttonSave: UIButton!
+    @IBOutlet weak var buttonDelete: UIButton!
+    @IBOutlet weak var buttonNew: UIButton!
+    
+    @IBOutlet weak var numberRecord: UITextView!
+    
+    var albums: NSMutableArray?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        albums = NSMutableArray(contentsOfFile:"Albums");
         // Do any additional setup after loading the view, typically from a nib.
+        let album = albums[0]
+        
+        artistTextField.text = album["artist"] as? String
     }
     
 // MARK: Actions
